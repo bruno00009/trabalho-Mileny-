c8d493d0c9d54606b45f8d3ed780a58129d85e37
bruno00009 <161394551+bruno00009@users.noreply.github.com>
2024-11-27 22:08:32 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,28 @@
+criaCartao(
+    'Programação',
+    'Quem sou eu?',
+    'Mileny'
+)
+
+criaCartao(
+    'Geografia',
+    'Meu trabalho é?',
+    'Muito bom'
+)
+
+criaCartao(
+    'Programação',
+    'Oque eu mais gosto no meu trabalho é?',
+    'As pesssoas com quem trabalho'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Nos finais de semana prefiro',
+    'Ficar com minha filha'
+)
+criaCartao(
+    'Lingua inglesa',
+    'Cor favorita',
+    'Azul'
+)
